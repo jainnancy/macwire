0829ce4c24a0d4b82640971947fec59d4f51a790
jainnancy <nancy.jain@knoldus.com>
2019-06-07 10:21:49 +0530

    Added case classes for object graph

---
+++ src/main/scala/com/knoldus/knolx/ManualDependencyForOG.scala
@@ -0,0 +1,37 @@
+package com.knoldus.knolx
+
+class ManualDependencyForOG {
+
+  case class Water()
+
+  case class CoffeeBeans()
+
+  case class Milk()
+
+  trait Coffee
+
+  case class Espresso(water: Water, coffeeBeans: CoffeeBeans) extends Coffee
+
+  case class Cappuccino(water: Water, milk: Milk, coffeeBeans: CoffeeBeans) extends Coffee
+
+  case class Latte(water: Water, coffeeBeans: CoffeeBeans, milk: Milk) extends Coffee
+
+  case class Tea(water: Water, milk: Milk)
+
+  case class CoffeeMachine(milk: Milk,
+                           coffee: List[Coffee],
+                           tea: Tea) {
+    println(s"Hello! Your Coffee Machine $this is ready.")
+  }
+
+  val water = new Water
+  val coffeeBeans = new CoffeeBeans
+  val milk = new Milk
+  val espresso = new Espresso(water, coffeeBeans)
+  val cappuccino = new Cappuccino(water, milk, coffeeBeans)
+  val latte = new Latte(water, coffeeBeans, milk)
+  val coffees = List(espresso, cappuccino, latte)
+  val tea = new Tea(water, milk)
+
+  val coffeeMachine = new CoffeeMachine(milk, coffees, tea)
+}
